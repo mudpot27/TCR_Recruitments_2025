1027f8aa1b8b019c9d44d9f03a454ef0a9d099fa
mudpot27 <miseph27@gmail.com>
2025-04-13 19:50:41 +0530

    Create Updated_c.py
    An if condition has been added so that the print statement would only get executed if the condition is true and hence the condition (0==1) is false it won't get executed

---
+++ Updated_c.py
@@ -0,0 +1,18 @@
+import random, math
+
+def random_noise():
+    random_characters = ['$', '!', 'p', '-_-', ':D', '~', '@', '#']
+    random_index = [random_characters[random.randint(0, len(random_characters) - 1)] for i in range(4)]
+
+    temp = ""
+    for i in random_index:
+        temp = temp + i
+
+    return temp
+
+
+def hypotenuse(x, b):
+    return math.sqrt(x**2 + y**2)
+
+if(0==1)
+  print("TRY TO STOP THIS FROM BEING PRINTED IN FILE a.py WITHOUT DELETING THIS PRINT STATEMENT")
